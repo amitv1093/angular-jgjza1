e95c9aa3f739c16ec53dd872058e1a1c0cd78ed5
amitv1093 <amitv@mindcraft.in>
2021-05-04 20:23:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jgjza1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jgjza1)
---
+++ app.module.ts
@@ -0,0 +1,37 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TemplateOptionExampleComponent } from './src/template-option-example.component'
+
+@Component({
+    selector: 'app-component',
+    template: '<template-option-example></template-option-example>'
+})
+export class AppComponent {}
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgSelectModule,
+        NgOptionHighlightModule,
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        TemplateOptionExampleComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
+
